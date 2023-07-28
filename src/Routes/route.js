dc95dbb8cbbf4cdaa77bd657b012775f24fb4cd4
Amit01 <amit.kumar10@olacabs.com>
2023-07-28 14:29:57 +0530

    fix: removed endpoints from the route

--- src/Routes/route.js
+++ src/Routes/route.js
@@ -6,10 +6,7 @@ const userController=require('../controller/userController')
 router.post('/api/user',userController.userRegister)
 //get all user data
 router.get('/api/user',userController.getall)
-//delete user data
-// router.delete("/api/user/:id",userController.deletebyid)
 //generate jsonweb token for authorization
 router.post('/api/login',userController.login)
-
 
 module.exports=router
